d85ff361ef7f13e36b0174aa8e5e694518051024
DaysOfApril <15002963354@163.com>
2018-04-04 17:29:50 +0800

    CollectionTest

--- src/com/book/collection/CollectionTest.java
+++ src/com/book/collection/CollectionTest.java
@@ -1,9 +1,44 @@
 package com.book.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
 public class CollectionTest 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Jillie is a lovely girl");
+		Collection<String> c = new ArrayList<>();
+		
+		c.add("Jillie");
+		c.add("is");
+		c.add("a");
+		c.add("lovely");
+		c.add("girl");
+		c.add("emmm");
+		c.remove("emmm");
+		
+		System.out.println("c: " + c);
+		System.out.println("集合元素个数为：" + c.size());
+		System.out.println("c.contains(\"Jillie\"): ? " + c.contains("Jillie") + "\n");
+		
+		
+		Collection<Object> set = new HashSet<>();
+		
+		set.add("Chen");
+		set.add("grace");
+		set.add("girl");
+		
+		System.out.println("set: " + set);
+		System.out.println("c是否完全包含set集合？ " + c.containsAll(set));
+		
+		c.removeAll(set);
+		System.out.println("c after delete set: " + c);
+		
+		c.clear();
+		System.out.println("c after clear: " + c);
+		
+		set.retainAll(c);
+		System.out.println("set after set.retainAll(c): " + set);
 	}
 }
